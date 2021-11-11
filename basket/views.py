2b3f2134fcd4e494219b731d1904edfcf5c4b0bd
lonneke-dev <lonneke1908@outlook.com>
2021-11-11 10:07:51 +0000

    Added adjust/remove from basket and calc subtotal

--- basket/views.py
+++ basket/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
 
 def view_basket(request):
     """ A view to return the basket contents page """
-    template = 'basket/basket.html'
-    return render(request, template)
+
+    return render(request, 'basket/basket.html')
 
 
 def add_to_basket(request, item_id):
@@ -23,3 +23,33 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    basket = request.session.get('basket', {})
+
+    try:
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
