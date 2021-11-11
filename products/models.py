a1e42e24ae242e7503f306a6429eca6688fe2fd4
lonneke-dev <lonneke1908@outlook.com>
2021-11-11 23:38:44 +0000

    Deploying to heroku

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    pot_size = models.DecimalField(max_digits=6, decimal_places=2)
-    height = models.DecimalField(max_digits=6, decimal_places=2)
+    pot_size = models.DecimalField(max_digits=2, decimal_places=2)
+    height = models.DecimalField(max_digits=2, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
