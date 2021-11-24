4ae607ce120ed8a3c3875730407944115f2ec4c0
lonneke-dev <lonneke1908@outlook.com>
2021-11-24 22:51:00 +0000

    Secret key Django

--- plantastic/settings.py
+++ plantastic/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i3gghmhugd)8-6e1rxj+*)ogovg8726)0_)1qtg+$^nk118%s4'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['plantastic-lon.herokuapp.com', 'localhost']
 
