3c91a2c2b56ce6d30de5cb128fa459378ead08bb
lonneke-dev <lonneke1908@outlook.com>
2022-01-09 20:07:15 +0000

    Made subscribe functionality working

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.newsletter, name='newsletter'),
-    path('mail_letter/', views.mail_letter, name='mail-letter')
+    path('mail_letter/', views.mail_letter, name='mail_letter')
 ]
