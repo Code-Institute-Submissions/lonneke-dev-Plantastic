70d30a82627a9aba0665c10c302e7605e197843b
lonneke-dev <lonneke1908@outlook.com>
2022-01-09 16:16:24 +0000

    Created newsletter app

--- plantastic/settings.py
+++ plantastic/settings.py
@@ -145,7 +145,7 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
